6869b24b5058712146175921d573b5eaef8728b5
ytaustin <31577602+ytaustin@users.noreply.github.com>
2018-11-06 15:08:30 -0600

    Snetiment analysis, using nrc to compare the emotion differnt between two president's tweets. compareed 3 lexicons

--- SentimentAnalysis.R
+++ SentimentAnalysis.R
@@ -25,9 +25,34 @@ tweets <- bind_rows(tweets_trump %>%
   mutate(created = ymd_hms(created))
 
 
+tweets_trump <- mutate(tweets_trump, person = "Trump") %>%
+  mutate(tnumber = row_number()) %>%
+  mutate(created = ymd_hms(created))
+
+tweets_obama <- mutate(tweets_obama, person = "Obama") %>%
+  mutate(tnumber = row_number()) %>%
+  mutate(created = ymd_hms(created))
 
 #clean the data a little bit
 remove_reg <- "&amp;|&lt;|&gt;|#"
+tidy_tweets_trump <- tweets_trump %>%
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_remove_all(text, remove_reg)) %>%
+  unnest_tokens(word, text, token = "tweets") %>%
+  filter(
+    !word %in% stop_words$word,!word %in% str_remove_all(stop_words$word, "'"),!str_detect(word, "president"),!str_detect(word, "trump"),!str_detect(word, "obama"),
+    str_detect(word, "[a-z]")
+  )
+
+tidy_tweets_obama <- tweets_obama %>%
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_remove_all(text, remove_reg)) %>%
+  unnest_tokens(word, text, token = "tweets") %>%
+  filter(
+    !word %in% stop_words$word,!word %in% str_remove_all(stop_words$word, "'"),!str_detect(word, "president"),!str_detect(word, "trump"),!str_detect(word, "obama"),
+    str_detect(word, "[a-z]")
+  )
+
 tidy_tweets <- tweets %>%
   filter(!str_detect(text, "^RT")) %>%
   mutate(text = str_remove_all(text, remove_reg)) %>%
@@ -36,6 +61,8 @@ tidy_tweets <- tweets %>%
     !word %in% stop_words$word,!word %in% str_remove_all(stop_words$word, "'"),!str_detect(word, "president"),!str_detect(word, "trump"),!str_detect(word, "obama"),
     str_detect(word, "[a-z]")
   )
+
+
 
 tidy_tweets_nrc <- tidy_tweets %>%
   inner_join(get_sentiments("nrc"))
@@ -47,7 +74,55 @@ ggplot(tidy_tweets_nrc, aes(x = sentiment, fill = person)) +
 ggsave("nrc_sentiment.jpg")
 
 
-tidy_tweets_afinn <- tidy_tweets %>%
-  inner_join(get_sentiments("afinn"))
+afinn_t <- tidy_tweets_trump %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = tnumber %/% 80) %>% 
+  summarise(sentiment = sum(score)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc_t <- bind_rows(tidy_tweets_trump %>% 
+                            inner_join(get_sentiments("bing")) %>%
+                            mutate(method = "Bing"),
+                          tidy_tweets_trump %>% 
+                            inner_join(get_sentiments("nrc") %>% 
+                                         filter(sentiment %in% c("positive", 
+                                                                 "negative"))) %>%
+                            mutate(method = "NRC")) %>%
+  count(method, index = tnumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn_t, 
+          bing_and_nrc_t) %>%
+  ggplot(aes(index, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
+ggsave("trumpsentiment.jpg")
+
+
+afinn_o <- tidy_tweets_obama %>% 
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = tnumber %/% 80) %>% 
+  summarise(sentiment = sum(score)) %>% 
+  mutate(method = "AFINN")
+
+bing_and_nrc_o <- bind_rows(tidy_tweets_obama %>% 
+                              inner_join(get_sentiments("bing")) %>%
+                              mutate(method = "Bing"),
+                            tidy_tweets_obama %>% 
+                              inner_join(get_sentiments("nrc") %>% 
+                                           filter(sentiment %in% c("positive", 
+                                                                   "negative"))) %>%
+                              mutate(method = "NRC")) %>%
+  count(method, index = tnumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+bind_rows(afinn_o, 
+          bing_and_nrc_o) %>%
+  ggplot(aes(index, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~method, ncol = 1, scales = "free_y")
+ggsave("obamasentiment.jpg")
 
 
